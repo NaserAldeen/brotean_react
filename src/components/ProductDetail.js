4b206e99c068c630d4360a5809854fd51e87b559
NaserAldeen <30509@aum.edu.kw>
2019-10-14 10:28:56 +0300

    some design

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { getProduct } from "../redux/actions";
 import { connect } from "react-redux";
+import QuantitySpinner from "./QuantitySpinner";
 class ProductDetail extends Component {
   componentDidMount() {
     this.props.getProduct(this.props.match.params.prodID);
@@ -12,27 +13,38 @@ class ProductDetail extends Component {
     return (
       <div className="container mt-5">
         <div className="row">
-          <div className="col-4">
-            <img
-              className="mt-5"
-              src={this.props.currentProduct.image}
-              style={{ width: "340px", height: "400px" }}
-            />
+          <div className="col-4 ml-5">
+            <div className="row">
+              <img
+                className="mt-5"
+                src={this.props.currentProduct.image}
+                style={{ width: "340px", height: "400px" }}
+              />
+            </div>
+            <div className="row">
+              <h5 className="" style={{ fontSize: "40px", marginLeft: "35px" }}>
+                Price: <strong>{this.props.currentProduct.price}KWD</strong>
+              </h5>
+            </div>
+            <div className="row">
+              <p>
+                <button class="btn btn-5 btn-5a icon-cart">Add to cart</button>
+              </p>
+            </div>
+            <div className="row">
+              <QuantitySpinner />
+            </div>
           </div>
-          <div className="col-6">
-            <h1 className="">{this.props.currentProduct.name}</h1>
+          <div className="col-6 ml-4">
+            <h1 className="" style={{ fontSize: "48px" }}>
+              {this.props.currentProduct.name}
+            </h1>
             <p className="mb-4 text-muted">
               {this.props.currentProduct.manufacturer ? "By: " : null}
               {this.props.currentProduct.manufacturer}
             </p>
             <p className="text-justify" style={{ lineHeight: "2" }}>
               {this.props.currentProduct.description}
-            </p>
-            <h5>
-              Price: <strong>{this.props.currentProduct.price}KWD</strong>
-            </h5>
-            <p>
-              <button class="btn btn-5 btn-5a icon-cart">Add to cart</button>
             </p>
           </div>
         </div>
